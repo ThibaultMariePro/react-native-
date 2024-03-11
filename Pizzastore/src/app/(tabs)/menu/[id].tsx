b56bcefde38058fa9a51a1d75bbafdc652e45f92
TIBO <thibault.marie.pro@gmail.com>
2024-03-11 09:40:56 +0100

    detail view of product getting data from product id

--- Pizzastore/src/app/(tabs)/menu/[id].tsx
+++ Pizzastore/src/app/(tabs)/menu/[id].tsx
@@ -1,23 +1,42 @@
 import { Stack, useLocalSearchParams } from "expo-router";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Image,  Text, StyleSheet } from "react-native";
+import products from "@assets/data/product";
 
 const ProductDetailItem = () => {
   const { id } = useLocalSearchParams();
+  const product = products.find((p) => p.id.toString() === id);
+  if (!product) {
+    return <Text>Product not found</Text>;
+  }
 
   return (
-    <View>
-      <Stack.Screen options={{ title: `Details of product ${id}` }} />
-      <Text style={styles.text}>{id}</Text>
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: product?.name }} />
+      <Image
+        source={{ uri: product.image }}
+        style={styles.image}
+        resizeMode="contain"
+      />
+      <Text style={styles.price}>{product.price}€</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  text: {
-    color: "white",
+  container:{
+    flex: 1,
+    backgroundColor: "white",
+    padding: 10,
+  },
+  price: {
+    color: "blue",
     fontSize: 20,
     fontWeight: "bold",
   },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  }
 });
 
 export default ProductDetailItem;
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # react-native-pizzastore
 learning react native following a React Native Full 8 Hours Course (Expo, Expo Router, Supabase)
+
+
+## [last stop in video](https://youtu.be/rIYzLhkG9TA?t=4810)
